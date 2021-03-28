a5f37dc2ceacdf812262d06500d178ef417947d7
alex.karamushko <alex.karamushko@nordclan.com>
2021-03-28 13:41:22 +0300

    add custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
 import React, { FC, ReactElement, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
 import routes from './routes';
+import theme from './theme';
 
 import '@assets/global.scss';
 
 const App: FC = (): ReactElement => (
-  <Router>
-    <Suspense fallback={<div />}>
-      <Switch>
-        <Route path="/" exact>
-          <Switch>
-            {routes.map((route) => (
-              <Route
-                key={route.path}
-                // eslint-disable-next-line react/jsx-props-no-spreading
-                {...route}
-              />
-            ))}
-          </Switch>
-        </Route>
-      </Switch>
-    </Suspense>
-  </Router>
+  <ThemeProvider theme={theme}>
+    <Router>
+      <Suspense fallback={<div />}>
+        <Switch>
+          <Route path="/" exact>
+            <Switch>
+              {routes.map((route) => (
+                <Route
+                  key={route.path}
+                  // eslint-disable-next-line react/jsx-props-no-spreading
+                  {...route}
+                />
+              ))}
+            </Switch>
+          </Route>
+        </Switch>
+      </Suspense>
+    </Router>
+  </ThemeProvider>
 );
 
 export default App;
